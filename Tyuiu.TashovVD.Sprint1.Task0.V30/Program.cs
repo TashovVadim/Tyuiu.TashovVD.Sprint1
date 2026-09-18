07a43c1a726a41f8938ac02da7b65f5da70707a5
Vadimas106 <tasovvadim412@gmail.com>
2026-09-18 21:34:31 +0500

    Add project files.

---
+++ Tyuiu.TashovVD.Sprint1.Task0.V30/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.TashovVD.Sprint1.Task0.V30.Lib;
+
+namespace Tyuiu.TashovVD.Sprint1.Task0.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #1 | Выполнил: Ташов В. Д. | АСОиУб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Ташов В. Д. | АСОиУб-26-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать контрольную программу, которая вычисляет 20 * 5 - 4            *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 20 * 5 - 4                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
