a8e4542040cc9734ee185f345e911d497bac06a8
Vadimas106 <tasovvadim412@gmail.com>
2026-09-19 23:19:23 +0500

    Task4

---
+++ Tyuiu.TashovVD.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.TashovVD.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.TashovVD.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x, y;
+            x = 5;
+            y = 4;
+            double wait = 0.143;
+            DataService ds = new DataService();
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TashovVD.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.TashovVD.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.TashovVD.Sprint1.Task4.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Ташов В. Д. | АСОиУб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Ташов В. Д. | АСОиУб-26-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.Write("Введите значение x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            var res = ds.Calculate(x, y);
+
+            Console.WriteLine($"1 / | x + y ^ 1/2 | =  " + res);
+            Console.ReadKey();
+        }
+    }
+}
